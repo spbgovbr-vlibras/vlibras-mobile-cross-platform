026d73e110cc31fd43ff08010eff92e2e50a4fac
caio.carvalho <caio.carvalho@lavid.ufpb.br>
2021-04-29 00:40:09 -0300

    Historic page 1.0

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -10,6 +10,8 @@ import LogoIcaro from './images/logo-icaro.png';
 import logoRealizadores from './images/realizadores1.png';
 import logoRnp from './images/rnp_logo.png';
 import vlibrasBackground from './images/suit-vlibras-background.png';
+import logoTranslator1 from './images/translate_logo1.png';
+import logoTranslator2 from './images/translate_logo2.png';
 import logoUfpb from './images/ufpb_logo.png';
 import logoAvatares from './images/vlibras-characters.png';
 import logoYoutube from './images/youtube_logo.png';
@@ -26,4 +28,6 @@ export {
   logoUfpb,
   logoYoutube,
   vlibrasBackground,
+  logoTranslator1,
+  logoTranslator2,
 };
--- src/pages/Historic/index.tsx
+++ src/pages/Historic/index.tsx
@@ -3,41 +3,96 @@ import React from 'react';
 import {
   IonButton,
   IonButtons,
+  IonChip,
   IonContent,
+  IonFooter,
   IonHeader,
   IonIcon,
   IonImg,
+  IonItem,
   IonPage,
   IonText,
+  IonTextarea,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import { shareSocial, menu } from 'ionicons/icons';
+import { menu, closeCircleOutline } from 'ionicons/icons';
 
+import { logoTranslator1, logoTranslator2 } from '../../assets';
 import { Strings } from './strings';
 import './styles.css';
 
 function Historic() {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar class="about-toolbar">
-          <IonTitle class="about-toolbar-title">
+      <IonHeader class="ion-no-border">
+        <IonToolbar class="historic-toolbar">
+          <IonTitle class="historic-toolbar-title">
             {Strings.TOOLBAR_TITLE}
           </IonTitle>
 
           <IonButtons slot="start">
             <IonButton>
-              <IonIcon icon={menu} />
+              <IonIcon icon={menu} class="historic-toolbar-icon" />
             </IonButton>
           </IonButtons>
           <IonButtons slot="end">
             <IonButton>
-              <IonIcon icon={shareSocial} />
+              <IonIcon
+                icon={closeCircleOutline}
+                class="historic-toolbar-icon-circleOutline"
+              />
             </IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
+      <div className="historic-container">
+        <div className="historic-container-ion-chips">
+          <IonChip class="historic-container-ion-chips-1">
+            {Strings.CHIP_TEXT_1}
+          </IonChip>
+          <IonChip class="historic-container-ion-chips-2">
+            {Strings.CHIP_TEXT_2}
+          </IonChip>
+          <IonChip class="historic-container-ion-chips-3">
+            {Strings.CHIP_TEXT_3}
+          </IonChip>
+        </div>
+        <div className="historic-container-ion-img-1">
+          <IonImg
+            src={logoTranslator1}
+            class="historic-container-ion-img-translator-1"
+          />
+          <IonText>{Strings.TRANSLATOR_TEXT_1}</IonText>
+        </div>
+        <div className="historic-container-box-ion-text">
+          <IonItem class="historic-container-box-ion-item">
+            <IonTextarea
+              placeholder={Strings.TEXT_AREA}
+              rows={10}
+              class="historic-container-box-ion-text-area"
+            />
+          </IonItem>
+        </div>
+        <div className="historic-container-ion-img-2">
+          <IonImg
+            src={logoTranslator2}
+            class="historic-container-ion-img-translator-2"
+          />
+          <IonText>{Strings.TRANSLATOR_TEXT_2}</IonText>
+        </div>
+        <div className="historic-container-ion-chips-box">
+          <IonChip class="historic-container-ion-chips-box-text-1">
+            {Strings.CHIP_TEXT__LIBRAS_1}
+          </IonChip>
+          <IonChip class="historic-container-ion-chips-box-text-2">
+            {Strings.CHIP_TEXT__LIBRAS_2}
+          </IonChip>
+          <IonChip class="historic-container-ion-chips-box-text-3">
+            {Strings.CHIP_TEXT__LIBRAS_3}
+          </IonChip>
+        </div>
+      </div>
     </IonPage>
   );
 }
