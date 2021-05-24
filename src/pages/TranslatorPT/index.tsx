95c5698b5d799ebcfa79d050103b8006b765df87
suanny <suanny@lavid.ufpb.br>
2021-05-23 23:57:27 -0300

    Video recorder (for testing)

--- src/pages/TranslatorPT/index.tsx
+++ src/pages/TranslatorPT/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import {
+  VideoCapturePlus,
+  VideoCapturePlusOptions,
+  MediaFile,
+} from '@ionic-native/video-capture-plus';
+
+import {
   IonContent,
   IonHeader,
   IonPage,
@@ -9,19 +15,35 @@ import {
   IonButton,
 } from '@ionic/react';
 
-import { RNCamera, FaceDetector } from 'react-native-camera';
-
 import { OnBoarding, VideoOutputModal } from 'components';
+import { File } from '@ionic-native/file';
 
 import { Strings } from './strings';
 import './styles.css';
 
 function TranslatorPT() {
+  const [metadata, setMetadata] = React.useState({});
+  const [videoSrc, setVideoSrc] = React.useState('');
+
+  async function takeVideo() {
+    try {
+      const options = { limit: 1, duration: 30 };
+      const mediafile = await VideoCapturePlus.captureVideo(options);
+      const fileResolved = await File.resolveLocalFilesystemUrl(
+        mediafile[0].fullPath.replace('/private', 'file://'),
+      );
+      setMetadata(mediafile);
+      setVideoSrc(fileResolved.nativeURL);
+    } catch (error) {
+      setMetadata(error);
+    }
+  }
+
   return (
     <IonPage>
       <IonContent>
         {/* <OnBoarding /> */}
-        <VideoOutputModal
+        {/* <VideoOutputModal
           outputs={[
             'febre',
             'hospital',
@@ -33,11 +55,18 @@ function TranslatorPT() {
             'tique-teco',
           ]}
           showButtons={true}
-        />
+        /> */}
 
         <div className="container">
           <div className="video-recording"></div>
-          <div className="area-recording"></div>
+          <div className="area-recording">
+            <button className="home-button-record" onClick={takeVideo}>
+              Opaa
+            </button>
+
+            <p className="home-vide-info">{JSON.stringify(metadata)}</p>
+            <p className="home-vide-info">{videoSrc}</p>
+          </div>
         </div>
       </IonContent>
     </IonPage>
