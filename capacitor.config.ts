7aecbfab465b08df05ac53092f55b7ac7465fde6
Marcos Alves <marcos.alves@lavid.ufpb.br>
2021-09-26 21:12:13 -0300

    Update build version iOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,20 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'lavid.ufpb.vlibras.mobile',
+  appId: 'br.ufpb.lavid.vlibras',
   appName: 'VLibrasMobile',
   webDir: 'build',
   bundledWebRuntime: false,
   cordova: {},
-  "server": {
-    "androidScheme": "http",
-    "allowNavigation": [
-      "lavid.nsa.root.sx"
-    ]
+  server: {
+    androidScheme: 'http',
+    allowNavigation: ['lavid.nsa.root.sx'],
   },
-  "plugins": {
-    "SplashScreen": {
-      "launchShowDuration": 0
-    }
-  }
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+    },
+  },
 };
 
 export default config;
