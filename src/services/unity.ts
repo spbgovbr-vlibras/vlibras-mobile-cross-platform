be3e5590c14202180bdb44ad62fe6fa955c1941e
Thiago Rocha <thiagofsr97@gmail.com>
2024-02-01 22:39:01 -0300

    Fixed regionalism by updating build.

--- src/services/unity.ts
+++ src/services/unity.ts
@@ -15,7 +15,7 @@ export default class UnityService {
 
   private constructor() {
     this.unityContent = new UnityContent(
-      'final/Build/22-12-2023 [SEM TRANSPARENCIA].json',
+      'final/Build/01-02-2024.json',
       'final/Build/UnityLoader.js',
       {
         adjustOnWindowResize: true,
