2072ebe711e370d4d16be6ebb952bcdba31bc0bd
Thiago Rocha <thiagofsr97@gmail.com>
2024-05-26 18:06:54 -0300

    Add error handling for regionalism.

--- src/pages/Regionalism/strings.ts
+++ src/pages/Regionalism/strings.ts
@@ -3,4 +3,5 @@ export const Strings = {
   INFO_BASIC: 'Escolha o regionalismo das traduções',
   BUTTON_SAVE: 'Salvar',
   BUTTON_CANCEL: 'Cancelar',
+  REGIONALISM_ERROR: 'Falha na conexão com o banco de sinais, não é possível sinalizar sinais regionais'
 };
